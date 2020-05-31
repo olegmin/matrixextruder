a923bb7cdb0f4950a528ce437765e130b64f4347
oleg <oleg_mnv@yahoo.com>
2020-05-31 22:46:34 +0300

    Preview of aiohttp client

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
-from aiohttp import web
+import asyncio
+from aiohttp import ClientSession
 
-print(f"\nHello, world!\n")
+
+URL = 'https://f003.backblazeb2.com/file/am-avito/matrix.txt'
+
+
+async def get_matrix(url: str):
+    async with ClientSession() as session:
+        async with session.get(URL) as resp:
+            result = await resp.text()
+    return result
+
+
+if __name__ == "__main__":
+    r = asyncio.run(get_matrix(URL))
+    print(r)
