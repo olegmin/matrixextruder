6ed52b709682035fdc3f29c31711cc5acd96a503
oleg <oleg_mnv@yahoo.com>
2020-06-01 00:34:29 +0300

    Create extract function in to the own module

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+# Necessary libraries
 import asyncio
 from matextr import get_matrix
 
 
+# Script configuration
 URL = 'https://f003.backblazeb2.com/file/am-avito/matrix.txt'
 
 
+# Launch the script
 if __name__ == "__main__":
     r = asyncio.run(get_matrix(URL))
     print(r)
