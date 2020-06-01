bd78f2465384744faaa2345ca3950eadb8402cbf
oleg <oleg_mnv@yahoo.com>
2020-06-01 13:54:17 +0300

    The correct offset mechanism is ready

--- src/matextr/load.py
+++ src/matextr/load.py
@@ -3,21 +3,53 @@
 """
 
 
-def load_data(matrix, flag=True, result=[]):
-    if len(matrix) <= 0:
-        return result
-    else:
-        print(f"Inserted matrix: {matrix}")
-        if flag:
-            transposed_matrix = list(zip(*matrix))
-            print(f"transposed matrix: {transposed_matrix}")
-            for item in transposed_matrix.pop(0):
-                result.append(item)
+def load_data(matrix):
+    mln = len(matrix)
+    coords = Сoordinates(mln)
+    result = []
+    for _ in range(mln ** 2):
+        x, y = coords.next()
+        result.append(matrix[x][y])
+    return result
+
+
+class Сoordinates:
+    def __init__(self, sqlnght: int):
+        self.square_length = sqlnght
+        self.offsets = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        self.visited = []
+        self.x = None
+        self.y = None
+        self.next_offset = 0
+
+    def next(self):
+        if self.x is None and self.y is None:
+            self.x, self.y = self._init()
         else:
-            transposed_matrix = list(zip(*matrix[::-1]))
-            print(f"transposed matrix: {transposed_matrix}")
-            dev = len(transposed_matrix) - 1
-            for item in transposed_matrix.pop(dev)[::-1]:
-                result.append(item)
-        print(f"matrix after pop: {transposed_matrix}\n")
-        return load_data(transposed_matrix, flag!=True)
+            next_x, next_y = self._bias()
+            if self._lost_course(next_x, next_y):
+                self._turn_left()
+                next_x, next_y = self._bias()
+            self.x, self.y = next_x, next_y
+        self.visited.append((self.x, self.y))
+        return (self.x, self.y)
+
+    def _init(self):
+        self.x, self.y = 0, 0
+        return (self.x, self.y)
+
+    def _bias(self):
+        inc = self.offsets[self.next_offset]
+        next_x = self.x + inc[0]
+        next_y = self.y + inc[1]
+        return next_x, next_y
+
+    def _lost_course(self, cx: int, cy: int):
+        return cx >= self.square_length or cx < 0 or \
+            cy >= self.square_length or cy < 0 or \
+            (cx, cy) in self.visited
+
+    def _turn_left(self):
+        self.next_offset += 1
+        if self.next_offset >= len(self.offsets):
+            self.next_offset = 0
